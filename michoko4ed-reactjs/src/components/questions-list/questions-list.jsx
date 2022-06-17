1f15e53d13a61237c3a04c541dc391afdbb7e5aa
sharon bello <sharonbellotech@gmail.com>
2022-06-17 20:43:58 +0300

    question-list

--- michoko4ed-reactjs/src/components/questions-list/questions-list.jsx
+++ michoko4ed-reactjs/src/components/questions-list/questions-list.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { QuestionPreview } from '../questions-preview/questions-preview.jsx'
 
-export const QuestionsList = () => (
-  <div>
-    Questions-list Component
-  </div>
-);
-
+export const QuestionsList = ({ questions }) => {
+  return (
+    <div className="list-of-questions-container">
+      <ul className="question-list clean-list">
+        {questions.map(question =>
+          <QuestionPreview
+            key={question._id}
+            question={question}
+          />
+        )}
+      </ul>
+    </div>
+  )
+}
